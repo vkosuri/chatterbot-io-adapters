468fc627135e33beaa5bfe8000fc72645a2b1a76
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2017-10-07 06:01:49 +0530

    io adapters

---
+++ chatterbot_io_adapters/__init__.py
@@ -0,0 +1,7 @@
+"""
+ChatterBot's Input and Output adapters that will integrate with chatbot.
+"""
+__version__ = '0.0.1'
+__author__ = 'Mallikarjunarao Kosuri'
+__email__ = 'malli.kv2@gmail.com'
+__url__ = 'https://github.com/vkosuri/chatterbot-adapters'
